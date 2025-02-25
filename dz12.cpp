4ca0d042807d412ff11f11f99c798b61a183ea64
Бордюжа Марк <borduzamark@gmail.com>
2025-02-25 23:37:55 +0200

    Add files via upload

---
+++ dz12.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    const size_t SIZE = 10;
+    int arr[SIZE] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    int evenSum = 0;
+    int oddSum = 0;
+
+    for (size_t i = 0; i < SIZE; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            evenSum += arr[i];
+        }
+        else
+        {
+            oddSum += arr[i];
+        }
+    }
+
+    std::cout << "Even numbers: " << evenSum << "\n";
+    std::cout << "Odd numbers: " << oddSum << std::endl;
+
+    return 0;
+}
